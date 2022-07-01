be67bedaf30e7ad810faa5de0440ca90fcb283e9
0jyyyyy <ojy0533@naver.com>
2022-07-01 11:11:44 +0900

    Initial commit

---
+++ src/main/java/com/ojy/bookboard/BookBoardApplication.java
@@ -0,0 +1,13 @@
+package com.ojy.bookboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ojy/bookboard/BookBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ojy.bookboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
